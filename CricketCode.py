fa0d7499444a0163b9bc7b5edba8bbdb14786396
Bradford-Bot~ <StarrlightGhost@gmail.com>
2024-04-04 02:00:42 -0500

    Testing

--- CricketCode.py
+++ CricketCode.py
@@ -1,1 +1,10 @@
 print("Welcome to cricket!")
+import time
+print("Hello. My name is Cricket, I am a pre-programed algorithm that will walk you through any questions you have. I am in no way insulting or rude. I care for my users and I'd like to walk you through any questions you have.")
+print()
+question = input("What can I do for you?").lower()
+print("A: Calculator.")
+if question == "a":
+    print()
+    print("Yes")
+time.sleep(3)
